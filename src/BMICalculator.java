8c2ffd8eb36538ffcdb30ff1683caaf37c7276de
Yen-Yi Wu <jessicayywu@yahoo.com.tw>
2014-03-25 15:53:37 +0800

    8. Event-Driven
    Errors solved

--- src/BMICalculator.java
+++ src/BMICalculator.java
@@ -9,21 +9,21 @@ public class BMICalculator extends JFrame {
 	private JTextField jtfAge = new JTextField();
 	private JTextField jtfWeight = new JTextField();
 	private JTextField jtfHeight = new JTextField();	
-	
+
 	private JButton jbtEnter = new JButton("Enter");
 
 	private JLabel jlBMI = new JLabel("(BMI)");
 	private JLabel jlStatus = new JLabel("(Status)");
-	
+
 	private String[] unitOfWeight = {"pounds", "kilograms"};
-	private String[] unitOfWeight = {"inches", "meters"};
-	
+	private String[] unitOfHeight = {"inches", "meters"};
+
 	private JComboBox jcboWeight = new JComboBox(unitOfWeight);
-	private JComboBox jcboHeight = new JComboBox(unitOfWeight);
-	
+	private JComboBox jcboHeight = new JComboBox(unitOfHeight);
+
 	public BMICalculator() {		
 
-		
+
 		JPanel p1 = new JPanel(new GridLayout(3,2,5,5));
 		p1.add(new JLabel("   First Name: "));
 		p1.add(jtfFirstName);
@@ -31,7 +31,7 @@ public class BMICalculator extends JFrame {
 		p1.add(jtfLastName);
 		p1.add(new JLabel("   Age: "));
 		p1.add(jtfAge);
-		
+
 		JPanel p2 = new JPanel(new GridLayout(2,3,5,5));
 		p2.add(new JLabel(" *Weight: "));
 		p2.add(jtfWeight);
@@ -40,7 +40,7 @@ public class BMICalculator extends JFrame {
 		p2.add(jtfHeight);
 		p2.add(jcboHeight);
 		p2.setBorder(new TitledBorder("* Required field."));
-		
+
 		JPanel p3 = new JPanel(new GridLayout(3,2,5,5));
 		p3.add(new JLabel());
 		p3.add(jbtEnter);
@@ -48,50 +48,43 @@ public class BMICalculator extends JFrame {
 		p3.add(jlBMI);
 		p3.add(new JLabel("    Status: "));
 		p3.add(jlStatus);
-		
+
 		setLayout(new GridLayout(3,1,5,5));
 		add(p1);
 		add(p2);
 		add(p3);
-		
+
 		jbtEnter.addActionListener(new ButtonListener());
-		jcboWeight.addItemListener(new ItemListener() {
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				double usersWeight = Double.parseDouble(jtfWeight.getText());
-				if (jcboWeight.getSelectedIndex() == 0)
-					usersWeight *= 0.45359237;
-			}
-		});
-		jcboHeight.addItemListener(new ItemListener() {
-			@Override
-			public void itemStateChanged(ItemEvent e) {
-				double usersHeight = Double.parseDouble(jtfHeight.getText());
-				if (jcboHeight.getSelectedIndex() == 0)
-					usersHeight *= 0.0254;
-			}	
-		});
 	}
-	
+
 	public class ButtonListener implements ActionListener {		
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (jtfWeight.getText().equals("") || jtfHeight.getText().equals(""))
 				JOptionPane.showMessageDialog(null, "Some required fields are not filled.", "Error", JOptionPane.ERROR_MESSAGE);
 			else {
+				double usersWeight = Double.parseDouble(jtfWeight.getText());
+				double usersHeight = Double.parseDouble(jtfHeight.getText());
+
+				if (jcboWeight.getSelectedIndex() == 0)
+					usersWeight *= 0.45359237;
+
+				if (jcboHeight.getSelectedIndex() == 0)
+					usersHeight *= 0.0254;
+
 				double usersBMI = getBMI(usersWeight, usersHeight);
 				String usersStatus = getStatus(usersBMI);
-				
+
 				jlBMI.setText(String.format("%.2f", usersBMI));
 				jlStatus.setText(String.format("%s", usersStatus));	
 			}
 		}
-		
+
 		public double getBMI(double weight, double height) {
 			double bmi = weight / (Math.pow(height, 2));
 			return Math.round(bmi * 100) / 100.0;
 		}
-		
+
 		public String getStatus(double bmi) {
 			if (bmi < 18.5)
 				return "Underweight";
@@ -102,9 +95,9 @@ public class BMICalculator extends JFrame {
 			else
 				return "Obese";
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		BMICalculator frame = new BMICalculator();
 		frame.setSize(300,300);
@@ -112,6 +105,6 @@ public class BMICalculator extends JFrame {
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
+
 	}
 }
